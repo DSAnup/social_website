ba824b1fb1471229bd3660f6b26093f94e470fc8
dsanup <anup12.m@gmail.com>
2024-03-22 22:20:31 +0600

    add followers

--- extendprofile/urls.py
+++ extendprofile/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('login_cancel/', views.login_cancel, name='login_cancel'),
     path('register/', views.register, name='register'),
     path('users/', views.user_list, name='user_list'),
+    path('users/follow/', views.user_follow, name='user_follow'),
     path('users/<username>', views.user_detail, name='user_detail'),
 ]
